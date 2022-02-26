bab7e0ada39f4aaa645d1b3f657216ff2cdbe895
Steven Saus <steven@stevesaus.com>
2022-02-26 12:11:58 -0500

    Tweaks

--- sbagen_select.sh
+++ sbagen_select.sh
@@ -11,9 +11,13 @@
 
 SBAGenDir="/home/steven/apps/sbagen-1.4.4/"
 OGGDir="/home/steven/apps/sbagen-1.4.4/"
-
+sbgfile=""
 sbgfile=$(fdfind . ${SBAGenDir} --follow --type file --extension sbg | fzf --no-hscroll -m --height 90% --border --ansi --no-bold --header="Choose SBA file" --preview='sed -n "/^#/p" {}')
-
+oggfile=""
 oggfile=$(fdfind . ${SBAGenDir} --follow --type file --extension ogg | fzf --no-hscroll -m --height 90% --border --ansi --no-bold --header="Choose background" )
 
-/usr/bin/xterm -e /usr/bin/padsp_32 /home/steven/apps/sbagen-1.4.4/sbagen -m  "${oggfile}" "${sbgfile}" &
+if [ -z "${oggfile}" ];then
+    /usr/bin/xterm -e /usr/bin/padsp_32 /home/steven/apps/sbagen-1.4.4/sbagen "${sbgfile}" &
+else
+    /usr/bin/xterm -e /usr/bin/padsp_32 /home/steven/apps/sbagen-1.4.4/sbagen -m "${oggfile}" "${sbgfile}" &
+fi
