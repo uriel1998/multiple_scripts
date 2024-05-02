dad9d024c3de2c8f83d0d9cff25e143328d615e4
Steven Saus <steven@stevesaus.com>
2024-05-02 01:20:17 -0400

    plan

--- books_search_rewrite_plan.sh
+++ books_search_rewrite_plan.sh
@@ -1,1 +1,6 @@
 
+
+Get ID
+get book(s) to search in as before.
+use rg (or the zip version?) to search inside those selected files for what we want
+output the appropriate passage, maybe even open book file to that point (dunno how to do that!)
