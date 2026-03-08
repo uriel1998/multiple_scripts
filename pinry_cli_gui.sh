64c53ed117a9c10a785dd26d155bcfd622986bdc
Steven Saus <steven@stevesaus.com>
2026-03-08 00:09:19 -0500

    updated pinry-cli-gui to use ai tools for description if possible, and to use exiftool data for tags or description if present

--- pinry_cli_gui.sh
+++ pinry_cli_gui.sh
@@ -4,33 +4,70 @@
 # Create the YAD form
 process_file () {
     local file="${1}"
-    
-    TEMPFILE3=$(mktemp)    
-    convert "${file}" -resize "600x600" "${TEMPFILE3}"
-    result=$(yad --form --title="Enter Information" \
-        --geometry=650x610 \
-        --image="${TEMPFILE3}" \
-        --field="Tags":TXT "" \
-        --field="Board":TXT "maps" \
-        --columns=2 \
-        --width=650 \
-        --height=620)
-    # Check if the form was canceled or closed
-    if [[ -z "$result" ]]; then
-        echo "Dialog was canceled."
-    else
-        # Parse the result
-        board=$(echo "$result" | awk -F '|' '{print $2}')
-        tags=$(echo "$result" | awk -F '|' '{print $1}')
-
-        # Display the entered values
-        echo "Board: $board"
-        echo "Tags: $tags"
-        file=$(echo $(realpath "${file}"))
-        /home/steven/.local/bin/pinry add "${file}" --board "${board}" --tags "${tags}"
-    fi
-    rm "${TEMPFILE3}"
+    exists=$(grep -i "${HOME}/.local/share/pinrycli/uploaded_to_pinry" -e "${file}" -c)
+    if [[ "$exists" == "0" ]];then
+		if [ -f $(which exiftool) ];then
+			taglist=""
+			taglist=$(exiftool "${1}" | grep Subject | awk -F ':' '{print $2}' | xargs )
+			# xargs trims beginning, ending whitespace from tags
+			description=$(exiftool "${1}" | grep Comment | awk -F ':' '{print $2}')
+		fi
+		# skip assets
+		if [[ "$taglist" != *"asset"* ]];then 
+			TEMPFILE3=$(mktemp)    
+			convert "${file}" -resize "600x600" "${TEMPFILE3}"
+			if [ -f $(which ai_gen_alt_text.sh) ] && [[ "${description}" == "" ]];then 
+				description=$(ai_gen_alt_text.sh "${TEMPFILE3}" "Describe this in one sentence using no punctuation and not mentioning it is an image" | sed -e 's/ "/ “/g' -e 's/" /” /g' -e 's/"\./”\./g' -e 's/"\,/”\,/g' -e 's/\."/\.”/g' -e 's/\,"/\,”/g' -e 's/"/“/g' -e "s/'/’/g" -e 's/ -- /—/g' -e 's/(/❲/g' -e 's/)/❳/g' -e 's/ — /—/g' -e 's/ - /—/g'  -e 's/ – /—/g' -e 's/ – /—/g' | hxunent -f)
+			fi
+			if [[ "$taglist" == "" ]] && [[ "$description" != "" ]];then
+				result=$(yad --form --title="Enter Information" \
+					--geometry=650x610 \
+					--columns=2 \
+					--width=650 \
+					--height=620 \
+					--image="${TEMPFILE3}" \
+					--field="Tags":TXT "${taglist}" \
+					--field="Board":TXT "maps" \
+					--field="Description":TXT "${description}" )
+			else
+				result="${taglist}|maps|${description}"
+			fi
+			# Check if the form was canceled or closed
+			if [[ -z "$result" ]]; then
+				echo "Dialog was canceled."
+			else
+				# Parse the result
+				if [[ "${file}" == *"[D|d]ay"* ]];then
+					taglist=echo "day,${taglist}"
+				fi
+				if [[ "${file}" == *"[N|n]ight"* ]];then
+					taglist=echo "night,${taglist}"
+				fi			
+				board=$(echo "$result" | awk -F '|' '{print $2}')
+				tags=$(echo "$result" | awk -F '|' '{print $1}')
+				desc=$(echo "${result}" | awk -F '|' '{print $3}'| sed -e 's/ "/ “/g' -e 's/" /” /g' -e 's/"\./”\./g' -e 's/"\,/”\,/g' -e 's/\."/\.”/g' -e 's/\,"/\,”/g' -e 's/"/“/g' -e "s/'/’/g" -e 's/ -- /—/g' -e 's/(/❲/g' -e 's/)/❳/g' -e 's/ — /—/g' -e 's/ - /—/g'  -e 's/ – /—/g' -e 's/ – /—/g' | hxunent -f)
+				# Display the entered values
+				echo "Board: ${board}"
+				echo "Tags: ${tags}"
+				echo "Description: ${desc}"
+				if [ -f $(which exiftool) ] && [[ "${desc}" != "" ]];then
+					exiftool -preserve -overwrite_original_in_place -comment="${desc}" "${file}"
+				fi
+				file=$(echo $(realpath "${file}")) 
+				outstring=$(printf "/home/steven/.local/bin/pinry add \"%s\" --board %s --tags \"%s\" --description \"%s\"" "${file}" "${board}" "${tags}" "${desc}")
+				echo "${outstring}"
+				eval "${outstring}"
+				echo "${file}" >> "${HOME}/.local/share/pinrycli/uploaded_to_pinry"
+			fi
+			rm "${TEMPFILE3}"
+		fi
+	else
+		echo "Already uploaded."
+	fi
 }
+
+
+### INPOINT
 
 
 # Check if $1 is passed
