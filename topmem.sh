1e63244fef43cc8d4a8497ccc435619d58792e27
Steven Saus <steven@stevesaus.com>
2026-07-29 10:08:03 -0400

    fixed these to use PID

--- topmem.sh
+++ topmem.sh
@@ -1,43 +1,31 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-ps aux | awk '{ for (i=11; i<=NF; i++) printf("%s ",$i)} {print ""}' |\
-while read i
-do
-    STRING=$(echo "$i" | awk -F ' ' '{print $1}' )
-    STRING=${STRING##*( )}
-    case "$STRING" in
-    [*) 
-        string2=$(echo "$STRING" | cut -c 2- | rev | cut -c 2- | rev)
-        ;;
-    -*)
-        string2=$(echo "$STRING" | cut -c 2- )
-        ;;
-    *)
-        string2=$(basename "$STRING")
-        ;;
-    esac
-    
-    case "$string2" in
-    bash*|python*)
-        string3=$(echo "$i" | awk -F ' ' '{print $2}' )
-        if [ -n "$string3" ];then
-            tmpcmd=$(basename $string2)
-        else
-            tmpcmd=$(echo $string2)
-        fi
-        ;;
-    *)
-        tmpcmd=$(echo $string2)
-        ;;
-    esac
-    echo "$tmpcmd"
-    
-done | sort | awk '!_[$0]++' |\
-while read q
-do
-    if [ -n "$q" ];then
-    cmdmem=$(ps -C "$q" --no-headers -o pmem | xargs | sed -e 's/ /+/g' | bc)
-    cmdcpu=$(ps -C "$q" --no-headers -o pcpu | xargs | sed -e 's/ /+/g' | bc)
-    echo "CPU $cmdcpu MEM $cmdmem CMD $q"
-    fi
-done | sort -r -s -n -k 4 | head -5
+ps -eo pid=,ppid=,pcpu=,rss=,comm= |\
+awk '
+function getroot(pid,    parentpid) {
+    parentpid = parent[pid]
+    while ((parentpid in cmd) && parentpid > 1) {
+        pid = parentpid
+        parentpid = parent[pid]
+    }
+    return pid
+}
+{
+    pid = $1
+    parent[pid] = $2
+    cpu[pid] = $3 + 0
+    rss[pid] = $4 + 0
+    cmd[pid] = $5
+}
+END {
+    for (pid in cmd) {
+        root = getroot(pid)
+        totalcpu[root] += cpu[pid]
+        totalrss[root] += rss[pid]
+    }
+    for (pid in cmd) {
+        if (getroot(pid) == pid) {
+            printf "CPU %.1f MEM %.1f CMD %s\n", totalcpu[pid], totalrss[pid] / 1024, cmd[pid]
+        }
+    }
+}' | sort -r -s -n -k 4 | head -5
